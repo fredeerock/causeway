f0d1a0fd8a82141fc2ff440d6f5ff30ff6e274db
Derick Ostrenko <dostrenko@cct.lsu.edu>
2015-08-13 12:46:37 -0500

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name causewayApp
+ * @description
+ * # causewayApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('causewayApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name causewayApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the causewayApp
+ */
+angular.module('causewayApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
