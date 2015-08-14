3853621ce0fad9c235ee0807764f0433ea54114a
Derick Ostrenko <dostrenko@cct.lsu.edu>
2015-08-14 14:00:42 -0500

    text loading

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,42 +1,48 @@
 // jshint devel:true
 
-function zoomOutUp(elem, iterations) {
-var transformOrigin = elem.style['transform-origin'];
+$("#causeway-text").load("../data/causeway.html", function() {
+	allow();
+});
 
-var keyframes = [
-  {
-  transform: 'none',
-  opacity: '1',
-  transformOrigin: 'center bottom',
-  // offset: 0
-  },
-  {
-  transform: 'scale3d(.475, .475, .475) translate3d(0, 60px, 0)',
-  opacity: '1',
-  transformOrigin: 'center bottom',
-  // offset: 0.5
-  },
-  {
-  transform: 'scale3d(.1, .1, .1) translate3d(0, -2000px, 0)',
-  opacity: '0',
-  transformOrigin: 'center bottom',
-  // offset: 1
-  }
-];
-
-var timing =
-  {
-  duration: 3000,
-  iterations: iterations,
-  easing: 'cubic-bezier(0.550, 0.055, 0.675, 0.190)'
-  };
-
-return elem.animate(keyframes, timing);
+function allow(){
+	$("span").click(function () {
+		$(this).css("background-color","yellow");
+	});
 }
 
-$("span").click(function () {
-$(this).css("background-color","yellow");
-});
+function zoomOutUp(elem, iterations) {
+	var transformOrigin = elem.style['transform-origin'];
+
+	var keyframes = [
+	  {
+	  transform: 'none',
+	  opacity: '1',
+	  transformOrigin: 'center bottom',
+	  // offset: 0
+	  },
+	  {
+	  transform: 'scale3d(.475, .475, .475) translate3d(0, 60px, 0)',
+	  opacity: '1',
+	  transformOrigin: 'center bottom',
+	  // offset: 0.5
+	  },
+	  {
+	  transform: 'scale3d(.1, .1, .1) translate3d(0, -2000px, 0)',
+	  opacity: '0',
+	  transformOrigin: 'center bottom',
+	  // offset: 1
+	  }
+	];
+
+	var timing =
+	  {
+	  duration: 3000,
+	  iterations: iterations,
+	  easing: 'cubic-bezier(0.550, 0.055, 0.675, 0.190)'
+	  };
+
+	return elem.animate(keyframes, timing);
+}
 
 // window.onload = zoomOutUp(document.getElementsByTagName("span"), Infinity);
 
