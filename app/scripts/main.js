90729ea8aed2d5c7c6455c1c8f7e247f1ed005e1
Derick Ostrenko <dostrenko@cct.lsu.edu>
2015-08-14 15:08:27 -0600

    its working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,44 +6,54 @@
 
 
 	$("span").click(function () {
-		$(this).css("background-color","yellow");
-		$(this).animate({left: '250px'});
-		// zoomOutUp(this, 1);
+		// $(this).animate({left: '250px'});
+		// $(this).animate({fontSize: '3em'}, "slow");
+
+		// $(this).animate({scale3d:'.475, .475, .475', translate3d: '0, 60px, 0'});
+		// $(this).animate({scale3d:'.1, .1, .1', translate3d: '0, -2000px, 0', speed: "slow"});
+
+		zoomOutUp(this, 1);
+				// $(this).css("background-color","yellow");
+
 	});
 
 	function zoomOutUp(elem, iterations) {
-		var transformOrigin = elem.style['transform-origin'];
+        var transformOrigin = elem.style['transform-origin'];
 
-		var keyframes = [
-		  {
-		  transform: 'none',
-		  opacity: '1',
-		  transformOrigin: 'center bottom',
-		  // offset: 0
-		  },
-		  {
-		  transform: 'scale3d(.475, .475, .475) translate3d(0, 60px, 0)',
-		  opacity: '1',
-		  transformOrigin: 'center bottom',
-		  // offset: 0.5
-		  },
-		  {
-		  transform: 'scale3d(.1, .1, .1) translate3d(0, -2000px, 0)',
-		  opacity: '0',
-		  transformOrigin: 'center bottom',
-		  // offset: 1
-		  }
-		];
+        var keyframes = [
 
-		var timing =
-		  {
-		  duration: 3000,
-		  iterations: iterations,
-		  easing: 'cubic-bezier(0.550, 0.055, 0.675, 0.190)'
-		  };
+            {
+              transform: 'none',
+              opacity: '1',
+              transformOrigin: 'center bottom',
+              // offset: 0
+            },
 
-		return elem.animate(keyframes, timing);
-	}
+            {
+              transform: 'scale3d(.475, .475, .475) translate3d(0, 60px, 0)',
+              opacity: '1',
+              transformOrigin: 'center bottom',
+              // offset: 0.5
+            },
+
+            {
+              transform: 'scale3d(.1, .1, .1) translate3d(0, -2000px, 0)',
+              opacity: '0',
+              transformOrigin: 'center bottom',
+              // offset: 1
+            }
+
+          ];
+
+        var timing = {
+          duration: 3000,
+          iterations: iterations,
+          easing: 'cubic-bezier(0.550, 0.055, 0.675, 0.190)'
+
+        };
+
+        return elem.animate(keyframes, timing);
+      }
 
 // window.onload = zoomOutUp(document.getElementsByTagName("span"), Infinity);
 
