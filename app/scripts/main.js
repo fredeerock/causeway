d2977d5f6c42e42c2b0f69828636743d87c01cc7
Derick Ostrenko <dostrenko@cct.lsu.edu>
2015-08-23 13:58:33 -0500

    works: filling forwads, words disappear

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,24 @@
 // jshint devel:true
+var a;
+$("#causeway-text").load("../data/causeway.html", function() {
+	// $('#causeway-text').html(function(index, oldHtml) {
+	//     return oldHtml.replace(/\b(\w+?)\b/g, '<span class="word">$1</span>')
+	// });
 
-$("#causeway-text").load("../data/causeway.txt", function() {
-	$('#causeway-text').html(function(index, oldHtml) {
-	    return oldHtml.replace(/\b(\w+?)\b/g, '<span class="word">$1</span>')
+	$("span").click(function () {
+		// console.log($(this).attr("id"));
+		console.log($(this));
+		a = $(this).attr("class");
+		console.log(a);
+		// console.log($("#causeway-text").find("10"));
+		var b = $("#causeway-text").find("10");
+		zoomOutUp(this	);
 	});
-
-	$("span").click(function () {zoomOutUp(this, 1);});
 });
 
-	function zoomOutUp(elem, iterations) {
+	function zoomOutUp(elem) {
         var transformOrigin = elem.style['transform-origin'];
+        var fillMode = elem.style['animation-fill-mode'];
 
         var keyframes = [
 
@@ -53,7 +62,8 @@ $("#causeway-text").load("../data/causeway.txt", function() {
 
         var timing = {
           duration: 3000,
-          iterations: iterations,
+          fill: 'forwards',
+          iterations: 1,
           easing: 'cubic-bezier(0.550, 0.055, 0.675, 0.190)'
 
         };
